9eb7070a2ae76f5581e35e90a3382aae716f2107
Zina <zinasaenko@hotmail.com>
2023-03-22 14:08:54 +0200

    fix bug

--- src/components/Slider/CarouselList/CarouselList.jsx
+++ src/components/Slider/CarouselList/CarouselList.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const CarouselList() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const CarouselList = () => {
+	return <div></div>;
+};
+
+export default CarouselList;
